83cc5302352bd7aec6b270b0942643974100ca64
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-21 18:36:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,9 @@ class Student
   attr_reader :id
 
   def initialize(name, grade, id=nil)
-    @name, @grade, @id = name, grade, id 
+    @name, @grade, @id = name, grade, id
   end
-  
+
+  self.creat_table
+
 end
